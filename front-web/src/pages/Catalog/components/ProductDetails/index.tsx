493835f5dc1a1131b4a2e14f9b078d0df49d95d9
Marcio Barcellos <marcio.barcellos@outlook.com>
2021-02-03 20:07:06 -0300

    Add product details

--- front-web/src/pages/Catalog/components/ProductDetails/index.tsx
+++ front-web/src/pages/Catalog/components/ProductDetails/index.tsx
@@ -1,10 +1,48 @@
 import React from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { ReactComponent as ArrowIcon } from '../../../../core/assets/images/arrow.svg';
+import { ReactComponent as ProductImage } from '../../../../core/assets/images/product.svg';
+import ProductPrice from '../../../../core/components/ProductPrice';
 import './styles.scss';
 
-const ProductDetails = () => (
-    <div className="">
+type ParamsType = {
+    productId: string;
+}
 
-    </div>
-);
+const ProductDetails = () => {
+    const { productId } = useParams<ParamsType>();
+
+    console.log(productId);
+
+    return (
+        <div className="product-details-container">
+            <div className="card-base border-radius-20 product-details">
+                <Link to="/products" className="product-details-goback">
+                    <ArrowIcon className="icon-goback" />
+                    <h1 className="text-goback">voltar</h1>
+                </Link>
+                <div className="row">
+                    <div className="col-6 pr-5">
+                        <div className="product-details-card text-center">
+                            <ProductImage className="product-details-image" />
+                        </div>
+                        <h1 className="product-details-name">
+                            Computador Desktop - Intel Core i7
+                    </h1>
+                        <ProductPrice price="3.779,00" />
+                    </div>
+                    <div className="col-6 product-details-card">
+                        <h1 className="product-description-title">
+                            Descrição do produto
+                    </h1>
+                        <p className="product-description-text">
+                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam inventore eos reiciendis doloribus? Exercitationem ab eligendi adipisci atque fugit a perspiciatis dolorum deleniti? Placeat rerum maiores ullam, dolorum labore corporis.
+                    </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
 
 export default ProductDetails;
